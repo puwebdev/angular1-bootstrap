725c6bf18b0923689a39a0d40d1a87bbb8aaa95f
puwebdev <pudge5515@gmail.com>
2017-05-02 00:40:23 +0200

    refactor withdraw aud page

--- assets/js/controllers/withdrawaud.js
+++ assets/js/controllers/withdrawaud.js
@@ -38,6 +38,8 @@ angular.module('app', ['ngTable'])
                     count: 10
                 }, {
                     total: $scope.withdrawals.length, 
+                    paginationMaxBlocks: 6,
+                    counts: [10, 25],
                     getData: function (params) {
                         $scope.data = $scope.withdrawals.slice((params.page() - 1) * params.count(), params.page() * params.count());
                         //$defer.resolve($scope.data);
@@ -57,23 +59,41 @@ angular.module('app', ['ngTable'])
     			if ($scope.withdraw_amount > 0) {
     				var item = {
     					amount: $scope.withdraw_amount,
-    					bank_account: 'https://webapp.digitalsurge.io/api/private/aud-bank-accounts/1/',
+    					bank_account: 'https://webapp.digitalsurge.io/api/private/aud-bank-accounts/' + $scope.bankAccount.id + '/',
     				};
-    				private_apis.AddAUDWithdraw(item);
+    				private_apis.AddAUDWithdraw(item, function(res) {
+                        if (res.getStatus) {
+                            var options = {style: "simple", message: "Your withdrawal request has been placed", type: "success", timeout: 0};
+                            jQuery('body').pgNotification(options).show();
 
-                    private_apis.GetAUDWithdrawals(function(data) {
-                        $scope.withdrawals = data.results;
-                        $scope.withdrawals.sort(function(a,b){return b.created.localeCompare(a.created)});
-                        var count = (data.count-1) / 10 + 1;
-                        for (var i = 2; i <= count; i++) {
-                            private_apis.GetAUDWithdrawalsWithPage(i, function(data1) {
-                                console.log(2);
-                                $scope.withdrawals = $scope.withdrawals.concat(data1.results);
+                            private_apis.GetAUDWithdrawals(function(data) {
+                                $scope.withdrawals = data.results;
                                 $scope.withdrawals.sort(function(a,b){return b.created.localeCompare(a.created)});
+                                buildTable();
+                                var count = (data.count-1) / 10 + 1;
+                                for (var i = 2; i <= count; i++) {
+                                    private_apis.GetAUDWithdrawalsWithPage(i, function(data1) {
+                                        $scope.withdrawals = $scope.withdrawals.concat(data1.results);
+                                        $scope.withdrawals.sort(function(a,b){return b.created.localeCompare(a.created)});
+                                        buildTable();
+                                    });
+                                }
                             });
+
+                            private_apis.GetBalance(function (data) {
+                                if (data) {
+                                    $scope.available_balance_aud = data.available.AUD;
+                                }
+                            });
+                        } else {
+                            options = {style: "simple", message: res.data[Object.keys(res.data)[0]], type: "error", timeout: 0};
+                            jQuery('body').pgNotification(options).show();
                         }
                     });
-    			}
+    			} else {
+                    var options = {style: "simple", message: "A valid number is required", type: "error", timeout: 0};
+                    jQuery('body').pgNotification(options).show();
+                }
     		}
 	// scope functions
 		// change output price value
@@ -89,15 +109,15 @@ angular.module('app', ['ngTable'])
 				}
 			};
 
-		// change date string to human readable
-			$scope.toDateReadable = function (dateString) {
-				let dateObj = new Date(dateString);
-				let monthNames = ["January", "February", "March", "April", "May", "June"
-					, "July", "August", "September", "October", "November", "December"];
-				let temp = dateObj.getFullYear() + '-' + (dateObj.getMonth() + 1) + '-' + dateObj.getDate() + ' ' + to2(dateObj.getHours()) + ':' 
-					+ to2(dateObj.getMinutes()) + ':' + to2(dateObj.getSeconds());
-				return temp;
-			};
+        // change date string to human readable
+            $scope.toDateReadable = function (dateString) {
+                let dateObj = new Date(dateString);
+                let monthNames = ["January", "February", "March", "April", "May", "June"
+                    , "July", "August", "September", "October", "November", "December"];
+                let temp = to2(dateObj.getHours()) + ':' 
+                    + to2(dateObj.getMinutes()) + ':' + to2(dateObj.getSeconds()) + ' ' + dateObj.getDate() + '-' + (dateObj.getMonth() + 1) + '-' + dateObj.getFullYear();
+                return temp;
+            };
 
 	// custom functions
 
